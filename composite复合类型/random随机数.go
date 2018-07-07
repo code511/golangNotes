7dadc77c4aaa8cadf086b1c6ace5b5be9ab536cf
Code511 <59872568@qq.com>
2018-07-07 18:35:14 +0800

    code511
    今天竟然是7月7,情人节了

---
+++ composite复合类型/random随机数.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+// 1) 伪随机数 :
+//就是使用math包中的rand函数
+func main(){
+//rand.Seed(1)  //(死种子)以 1为种子 ,导致每次重复
+rand.Seed(time.Now().UnixNano()) //(活种子)以当前本地时间(最小单位 纳秒)作为种子 ,保证随机数的唯一性
+	for i := 0; i < 10; i++ {
+		//println(rand.Int())  //打印最大位数
+		//println(rand.Intn(9999))  //打印9999以内的随机数
+		fmt.Println(fmt.Sprintf("%04v",rand.Int31n(9999)))//打印固定四位数的随机数
+	}
+	/* 2)真随机数 :
+	crypto/rand包中的函数    */
+}
---
+++ composite复合类型/值传递和指针传递.go
@@ -0,0 +1,22 @@
+package main
+/*	值传递和指针传递	:
+	1) 值传递 :
+	只使用该指针的值,不改变该指针的值*/
+func zcd(a int)  {
+	a = 888
+	println(a)
+}
+/*	2) 指针传递 :
+	把指针传递进形参,操作会改变该指针的值*/
+func zzcd(a *int)  {
+	*a = 777
+}
+func main(){
+	a := 666
+	//值传递 :
+	zcd(a)
+	println(a)	//打印外部 a
+	//指针传递 :
+	zzcd(&a)	//传递 a的指针
+	println(a)	//指针传递后,外部 a的值已发生改变
+}
